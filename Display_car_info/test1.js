7ffcb0c87be29061e885df8461a97af8cf73d1a3
phuongtrieu97coder <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-03-11 12:33:56 -0800

    Create test1.js

---
+++ Display_car_info/test1.js
@@ -0,0 +1,14 @@
+[id="block0"]{
+    text-align:center;
+}
+
+[id="block1"]{
+    display:grid;
+    grid-template-columns:400px 400px 400px;
+}
+
+
+[id="block1_1"] p, [id="block1_2"] p, [id="block1_3"] p{
+    font-size:35px;
+    
+}
