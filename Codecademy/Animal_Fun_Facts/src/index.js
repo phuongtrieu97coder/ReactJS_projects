abc7e63cd975c954b5c4488352924f11ebad7ae0
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-21 23:57:14 -0700

    Update index.js

--- Codecademy/Animal_Fun_Facts/src/index.js
+++ Codecademy/Animal_Fun_Facts/src/index.js
@@ -10,3 +10,44 @@ import ReactDOM from "react-dom";
 import "./style1.scss";
 import PropTypes from "prop-types";
 import $ from "jquery";
+
+
+
+const title = "Click an animal for a fun fact";
+
+const background = (
+  <img className="background" src={"/images/ocean.jpg"} alt="ocean" />
+);
+
+let images = [animals["dolphin"], animals["lobster"], animals["starfish"]];
+
+let displayFact = (event) => {
+  document.getElementById("fact").innerHTML = event.target.alt;
+};
+
+const animalFacts = (
+  <div>
+    <h1>{title}</h1>
+    {background}
+    <p id="fact"></p>
+
+    <div className="animals">
+      {images.map((img, index) => {
+        return (
+          <img
+            key={index}
+            className="animal"
+            src={img.image}
+            alt={img.facts}
+            role="button"
+            onClick={displayFact}
+          />
+        );
+      })}
+    </div>
+  </div>
+);
+
+var container1 = document.getElementById("root");
+const root_1 = createRoot(container1);
+root_1.render(animalFacts);
