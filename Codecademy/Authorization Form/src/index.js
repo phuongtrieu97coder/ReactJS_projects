7422b46b63d0b63c47b820540890ddda6c2c9556
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-22 13:33:25 -0700

    Create index.js

---
+++ Codecademy/Authorization Form/src/index.js
@@ -0,0 +1,55 @@
+import React,{useState,useEffect,useCallback,useMemo,
+useReducer,useContext,createContext,useRed,createRef,
+forwardRef,lazy,Suspense,Component,PureComponent,memo,
+startTransition}
+from "react";
+import {BrowserRouter,Routes,Route,Outlet,Link}
+from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom";
+import "./test1.scss";
+import PropTypes from "prop-types";
+import $ from "jquery";
+
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    return (
+      <div id="authorization">
+        <h1>Contact</h1>
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+var container1 = document.getElementById("block1"),
+root_1 = createRoot(container1);
+root_1.render(<Contact/>);
