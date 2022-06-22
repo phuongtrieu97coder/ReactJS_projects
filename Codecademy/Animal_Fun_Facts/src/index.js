cd48de95f7695bd95a7e1b85669a5f5b712acf0b
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-21 22:44:41 -0700

    Create index.js

---
+++ Codecademy/Animal_Fun_Facts/src/index.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect,useCallback,useMemo,
+useReducer,useContext,createContext,useRed,createRef,
+forwardRef,lazy,Suspense,Component,PureComponent,memo,
+startTransition}
+from "react";
+import {BrowserRouter,Routes,Route,Outlet,Link}
+from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom";
+import "./test1.scss";
+import PropTypes from "prop-types";
+import $ from "jquery";
