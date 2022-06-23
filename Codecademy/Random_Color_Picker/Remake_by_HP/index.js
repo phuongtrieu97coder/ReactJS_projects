8dcb6c07e7b8db8751a87a28ab793525a3b20f38
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-23 15:49:14 -0700

    Update index.js

--- Codecademy/Random_Color_Picker/Remake_by_HP/index.js
+++ Codecademy/Random_Color_Picker/Remake_by_HP/index.js
@@ -12,60 +12,98 @@ import PropTypes from "prop-types";
 import $ from "jquery";
 import {Button} from "./test1";
 
-class Random extends React.Component {
+
+
+class ErrorBoundaries extends React.Component{
     constructor(props){
         super(props);
-        this.state = {color:[0,255,0]}
+        this.state = {error:false};
     }
-    componentDidMount() {
-      this.applyColor();
-      if(document.body.style.backgroundColor === "rgb(255,255,255)"){
-        //rgb(255,255,255) is white color
-        $("h1").css({
-            color:"black"
-          })
-      }else{
-        $("h1").css({
-            color:"white"
-          })
-      }
+    static getDerivedStateFromError(error){
+        return console.log(`
+        Error: ${error}.
+        `);
     }
-  
-    formatColor(ary) {
-      return 'rgb(' + ary.join(',') + ')';
+    componentDidCatch(error,errorinfo){
+        return console.log(`
+        Error: ${error}.
+        Error Information: ${errorinfo}.
+        `);
     }
-  
-    applyColor() {
-      const color = this.formatColor(this.state.color);
-      document.body.style.background = color;
+    render(){
+        if(this.state.error === true){
+            return <h1>There were errors occurred 
+            while rendering component contents!!!</h1>
+        }else{
+            return this.props.children;
+        }
     }
-  
-    chooseColor() {
-      const random = [];
-      for (let i = 0; i < 3; i++) {
-        random.push(Math.floor(Math.random()*256));
-      }
-      return random;
-    }
+}
 
-    handleClick =()=>{
+
+
+
+class Random extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {color:[0,255,0]};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount(){
+        this.ApplyBGColor();
+
+        if(document.body.style.background === "rgb(255,255,255)"){
+            $("[id='color_title']").css({color:"black"})
+        }else{
+            $("[id='color_title']").css({color:"white"})
+        }
+    }
+    componentDidUpdate(){ //this method helps you update background color of your website after the color property of this.state has been updated.
+        this.ApplyBGColor(); 
+    }
+    FormatColor =(colorArray) =>{
+        return `rgb(${colorArray.join(",")})`;
+    }
+    ApplyBGColor = () => {
+        let bgcolor = this.FormatColor(this.state.color); 
+        document.body.style.background = bgcolor;
+
+    }
+    randomBGColor = () => {
+        var arr1 = [];
+        for(let i = 0; i< 3 ; i++){
+            arr1.push(Math.floor(Math.random()*256));
+        }
+        return arr1;
+    }
+    handleClick(){
         this.setState({
-            color:this.chooseColor()
-          })
+            color:this.randomBGColor()
+        })
     }
-  
-    render() {
-      return (
-        <div className="w3-center">
-          <h1>
-               Your color is {this.formatColor(this.state.color)}
-          </h1>
-          <Button onClick={this.handleClick}/>
-        </div>
-      );
+    render(){
+        return (
+           <ErrorBoundaries>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                    <React.StrictMode>
+                    <div className="w3-center">
+                            <h1 id="color_title">
+                                Your Background Color is: {this.FormatColor(this.state.color)}</h1>
+                            <Button refresh={this.handleClick}/>
+                    </div>
+                    </React.StrictMode>
+            </Suspense>
+           </ErrorBoundaries>
+        )
     }
-  }
+}
+
 var container1 = document.getElementById("block1"),
 root_1 = createRoot(container1);
-root_1.render(<Random />);
+root_1.render(<Random/>);
+
+
+
+
+
 
