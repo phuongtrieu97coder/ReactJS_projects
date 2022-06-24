403fc3d7f148d2c5f075fbc27a5809b0fa9428a6
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2022-06-24 12:15:09 -0700

    commit_06_24_2022_12:15:00:PM

--- Codecademy/Social_Network_For_Pets/src/Profile.js
+++ Codecademy/Social_Network_For_Pets/src/Profile.js
@@ -14,32 +14,66 @@ export class Profile extends React.Component {
 
 
   loadUserData = () => {
-     
-      this.fetchID = fetchUserData(this.props.username, (userData) => {
-  this.setState({ userData });
-});
+       this.setState({userData:null});
+
+       this.fetchID = fetchUserData(
+           this.props.username, 
+           (userData) => {this.setState({ userData });
+       });
   }
+
+  componentWillUnmount(){
+    cancelFetch(this.fetchID);
+  }
+
+
+  componentDidUpdate(prevProps){
+      if(this.props.username !== prevProps.username){
+          cancelFetch(this.fetchID)
+          this.loadUserData();
+      }
+  }
+
+
+
   render() {
-    const isLoading = this.state.userData === null? true: false;
+    const isLoading = this.state.userData === null; // true
 
     let className = 'Profile';
     if (isLoading) {
       className += ' loading';
     }
 
-    let name = isLoading === true ? 'Loading...': this.state.userData.name;
+    let name;
+    if(isLoading){
+       name = "Loading Pet profile...";
+    }else{
+       name = this.state.userData.name;
+    }
+
 
     let bio = isLoading === true ? 'Loading...': this.state.userData.bio;
 
+    let friends;
+    if(isLoading){
+       friends = [];
+    }else{
+       friends = this.state.userData.friends;
+    }
+
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+            {!isLoading && (
+    <img src={this.state.userData.profilePictureUrl} alt="" />
+  )}
+        </div>
         <div className="profile-body">
           <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
           <p>{bio}</p>
           <h3>My friends</h3>
-          <Userlist usernames={[]} onChoose={this.props.onChoose} />
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
         </div>
       </div>
     );
--- Codecademy/Social_Network_For_Pets/src/index.js
+++ Codecademy/Social_Network_For_Pets/src/index.js
@@ -1,59 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Profile } from './Profile';
-import { Directory } from './Directory';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUsername: null,
-    };
-    this.handleChoose = this.handleChoose.bind(this);
-    this.handleReturnToDirectoryClick = this.handleReturnToDirectoryClick.bind(
-      this
-    );
-  }
-
-  handleChoose(newUsername) {
-    this.setState({ currentUsername: newUsername });
-  }
-
-  handleReturnToDirectoryClick() {
-    this.setState({ currentUsername: null });
-  }
-
-  render() {
-    let body;
-    if (this.state.currentUsername) {
-      body = (
-        <Profile
-          username={this.state.currentUsername}
-          onChoose={this.handleChoose}
-        />
-      );
-    } else {
-      body = <Directory onChoose={this.handleChoose} />;
+import React,{useState,useEffect,useCallback,useMemo,
+  useReducer,useContext,createContext,useRed,createRef,
+  forwardRef,lazy,Suspense,Component,PureComponent,memo,
+  startTransition}
+  from "react";
+  import {BrowserRouter,Routes,Route,Outlet,Link}
+  from "react-router-dom";
+  import {createRoot} from "react-dom/client";
+  import ReactDOM from "react-dom";
+  import "./test1.scss";
+  import PropTypes from "prop-types";
+  import $ from "jquery";
+  import { Profile } from './Profile';
+  import { Directory } from './Directory';
+  
+  
+  
+  class App extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          currentUsername: null,
+        };
+      }
+    
+      handleChoose =(newUsername)=> {
+        this.setState({ currentUsername: newUsername });
+      }
+    
+      handleReturnToDirectoryClick = () => {
+        this.setState({ currentUsername: null });
+      }
+    
+      render() {
+        let body;
+        if (this.state.currentUsername) {
+          body = (
+            <Profile
+              username={this.state.currentUsername}
+              onChoose={this.handleChoose}
+            />
+          );
+        } else {
+          body = <Directory onChoose={this.handleChoose} />;
+        }
+    
+        return (
+          <div className="App">
+            <header>
+              <h1>PetBook</h1>
+    
+              <nav>
+                {this.state.currentUsername && (
+                  <button onClick={this.handleReturnToDirectoryClick}>
+                    Return to directory
+                  </button>
+                )}
+              </nav>
+            </header>
+    
+            <main>{body}</main>
+          </div>
+        );
+      }
     }
-
-    return (
-      <div className="App">
-        <header>
-          <h1>PetBook</h1>
-
-          <nav>
-            {this.state.currentUsername && (
-              <button onClick={this.handleReturnToDirectoryClick}>
-                Return to directory
-              </button>
-            )}
-          </nav>
-        </header>
-
-        <main>{body}</main>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+  
+  
+  var container1 = document.getElementById("block1"),
+  root_1 = createRoot(container1);
+  root_1.render(<App/>);
+  
+  
+  
+  
+  
