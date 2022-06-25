37f70560ab7dbdd33695809cda38595592eeacb9
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-25 16:19:05 -0700

    Update index.js

--- Codecademy/Video_Player/src/index.js
+++ Codecademy/Video_Player/src/index.js
@@ -15,14 +15,21 @@ class App extends React.Component {
     super(props);
 
     this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+  }
+
+  chooseVideo(newVideo){
+    this.setState({
+      src:VIDEOS[newVideo]
+    });
   }
   
   render() {
     return (
       <div>
         <h1>Video Player</h1>
-        <Menu />
-        <Video />
+        <Menu chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src}/>
       </div>
     );
   }
