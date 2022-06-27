958f7e671008f6ed1953fa6d1b5af4de2db707b4
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-27 13:57:21 -0700

    Update CopyCatContainer.js

--- Codecademy/Copycats/src/containers/CopyCatContainer.js
+++ Codecademy/Copycats/src/containers/CopyCatContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Copycat} from '../components/CopyCat';
+import {CopyCat} from '../components/CopyCat';
 
 const images = {
   copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
