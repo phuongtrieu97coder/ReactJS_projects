820874403964105ef0948e97dc721bcd855edc8e
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-06-27 14:35:02 -0700

    Update CopyCat.js

--- Codecademy/Copycats/src/components/CopyCat.js
+++ Codecademy/Copycats/src/components/CopyCat.js
@@ -4,26 +4,30 @@ import PropTypes from "prop-types";
 
 export class CopyCat extends React.Component {
   render() {
-  const {copying,toggleTape,copycat,quietcat,input,handleChange} = this.props;
+    const {copying,toggleTape,copycat,quietcat,value,handleChange,name} = this.props;
 
     return (
       <div style={styles.divStyles}>
         <h1 style={{ marginBottom: 80 }}>
-          Copy Cat {this.props.name ? this.props.name : "Tom"}
+          Copy Cat {name || "Tom"}
         </h1>
-        <input type="text" value={input} onChange={handleChange} />
+        <input type="text" value={value} onChange={handleChange} />
         <img
           alt="cat"
           src={copying ? copycat : quietcat}
           onClick={toggleTape}
           style={styles.imgStyles}
         />
-        <p>{copying === true && input}</p>
+        <p>{copying && value}</p>
       </div>
     );
   }
 }
 
 CopyCat.propTypes = {
+  copying:PropTypes.bool.isRequired,
+  toggleTape: PropTypes.func.isRequired,
+  value:PropTypes.string.isRequired,
+  handleChange:PropTypes.func.isRequired,
   name: PropTypes.string,
 };
